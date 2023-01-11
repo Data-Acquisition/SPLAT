226d771febf7c67c704ae1fa7cec7a6429c6c95d
mikechetv <106532734+mikechetv@users.noreply.github.com>
2023-01-11 16:57:57 +0300

    Delete src directory

--- src/wb/datbass/__init__.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine('postgresql+psycopg2://localhost:5432/postgres')
-Session = sessionmaker(bind=engine)
-session = Session()
